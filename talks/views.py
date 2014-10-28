bdb557f75c79514f8f54564ec45db2417c740085
Ivan Martinez <alcance.enlinea@gmail.com>
2014-10-27 20:40:16 -0700

    RestrictToUserMixin

--- talks/views.py
+++ talks/views.py
@@ -7,32 +7,26 @@ from braces.views import LoginRequiredMixin, PrefetchRelatedMixin
 from .models import TalkList
 
 
+class RestrictToUserMixin(object):
+    def get_queryset(self):
+        queryset = super(RestrictToUserMixin, self).get_queryset()
+        queryset = queryset.filter(user=self.request.user)
+        return queryset
+
+
 class TalkListListView(
     LoginRequiredMixin,
+    RestrictToUserMixin,
     ListView
 ):
     model = TalkList
 
-    def get_queryset(self):
-        return self.request.user.lists.all()
-
 
 class TalkListDetailView(
     LoginRequiredMixin,
+    RestrictToUserMixin,
     #PrefetchRelatedMixin,
     DetailView,
 ):
     model = TalkList
     prefetch_related = ('talks',)
-    '''
-    prefetch and select related
-    select related sql query go get user related to
-    tlaklists that are in queryset. is a sql with joins
-    you dont go back to the db
-
-    prefetch join two queries.
-    '''
-    def get_queryset(self):
-        queryset = super(TalkListDetailView, self).get_queryset()
-        queryset = queryset.filter(user=self.request.user)
-        return queryset
