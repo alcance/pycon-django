418eda593dd9f02e21dc0bd6ecefa7fe803a2d68
Ivan Martinez <alcance.enlinea@gmail.com>
2014-10-28 19:15:58 -0700

    os environ

--- survivalguide/wsgi.py
+++ survivalguide/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survivalguide.settings")
 
 from whitenoise.django import DjangoWhiteNoise
 
